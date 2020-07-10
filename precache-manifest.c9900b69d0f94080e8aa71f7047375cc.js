0337d60386cc1b6a543aacc778a777b846b085bd
arnavdesk <arnavdesk@gmail.com>
2020-07-11 03:27:28 +0530

    Updates

---
+++ precache-manifest.c9900b69d0f94080e8aa71f7047375cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a70b07ac0bff7a5e1fe2c712c6f00d",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "0f2e6d5778e6a8432779",
+    "url": "/cart/static/css/main.b1df30db.chunk.css"
+  },
+  {
+    "revision": "1f50ea013e17155c1ce8",
+    "url": "/cart/static/js/2.f925977b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.f925977b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2e6d5778e6a8432779",
+    "url": "/cart/static/js/main.bac50540.chunk.js"
+  },
+  {
+    "revision": "4927c2924af25d99dd24",
+    "url": "/cart/static/js/runtime-main.2c967d9a.js"
+  }
+]);
